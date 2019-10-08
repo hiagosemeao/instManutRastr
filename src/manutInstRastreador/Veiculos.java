873137f5eaee8448d4bec3d83f21cc9ea74c06db
hiagosemeao <hiagosemeao@gmail.com>
2019-10-08 08:52:31 -0300

    Nova Atualização

--- src/manutInstRastreador/Veiculos.java
+++ src/manutInstRastreador/Veiculos.java
@@ -1,6 +1,51 @@
 package manutInstRastreador;
 
 public class Veiculos {
+	private String placa;
+	private String modelo;
+	private int anoFab;
+	private String cor;
+	
+	
+	public String getPlaca() {
+		return placa;
+	}
+
+
+	public void setPlaca(String placa) {
+		this.placa = placa;
+	}
+
+
+	public String getModelo() {
+		return modelo;
+	}
+
+
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+
+
+	public int getAnoFab() {
+		return anoFab;
+	}
+
+
+	public void setAnoFab(int anoFab) {
+		this.anoFab = anoFab;
+	}
+
+
+	public String getCor() {
+		return cor;
+	}
+
+
+	public void setCor(String cor) {
+		this.cor = cor;
+	}
+
 
 	public Veiculos() {
 		// TODO Auto-generated constructor stub
